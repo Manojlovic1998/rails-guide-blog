fb4608e461efd9b092589a1c6662cac8850c22b9
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-04 23:28:33 +0200

    Add model on save validation to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum:10 }
 end
+
+# Active Record automatically defines model attributes for every
+# table column, so you don't have to declare those attributes in your
+# model file
